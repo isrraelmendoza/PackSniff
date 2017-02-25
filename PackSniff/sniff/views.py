8719fc3ca8211a70c106f05ed3f3d119e91c2cdb
Isrrael Mendoza <isrraelmendoza92@gmail.com>
2017-02-25 04:29:09 +0000

    html response

--- PackSniff/sniff/views.py
+++ PackSniff/sniff/views.py
@@ -1,7 +1,13 @@
 #a view is a simple function that returns some html
 from django.http import HttpResponse
+from .models import Packet as PacketModel
 
 def index(request):
+    all_IPs = PacketModel.objects.all()
+    html = ''
+    for IPs in all_IPs:
+        url = '/sniff/' 
+        html += '<a href="' + url  + '</a><br>'
     return HttpResponse('<h1>This is the IP sniffer app homepage<h1>')
     
 def detail(request, ip_address):
